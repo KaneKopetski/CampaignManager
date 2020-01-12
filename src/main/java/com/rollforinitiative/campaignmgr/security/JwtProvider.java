8fb26b1838eeca65357d0f3e188ed87a8ec1cd14
KaneKopetski <kane.kopetski@gmail.com>
2020-01-12 11:28:38 -0500

    rename keystore

--- src/main/java/com/rollforinitiative/campaignmgr/security/JwtProvider.java
+++ src/main/java/com/rollforinitiative/campaignmgr/security/JwtProvider.java
@@ -21,7 +21,7 @@ public class JwtProvider {
     public void init() {
         try {
             keyStore = KeyStore.getInstance("JKS");
-            InputStream resourceAsStream = getClass().getResourceAsStream("/springblog.jks");
+            InputStream resourceAsStream = getClass().getResourceAsStream("/roll4init.jks");
             keyStore.load(resourceAsStream, "secret".toCharArray());
         } catch (KeyStoreException | CertificateException | NoSuchAlgorithmException | IOException e) {
             throw new CampaignManagerException("Exception occurred while loading keystore");
